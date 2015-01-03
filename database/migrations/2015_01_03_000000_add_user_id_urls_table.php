4e02fb4ca907f9ad5935dc531980a74d8382e6a4
Noah Bass <bassjnoah@gmail.com>
2015-01-03 14:07:12 -0500

    added user_id column to urls table

---
+++ database/migrations/2015_01_03_000000_add_user_id_urls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdUrlsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('urls', function(Blueprint $table) {
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('urls', function(Blueprint $table)
+        {
+            $table->dropColumn('user_id');
+        });
+    }
+
+}
